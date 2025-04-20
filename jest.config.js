f3da5a09a84004d9d97113903615efdbe70387ad
Abel Lukwaro <abelmabruke@gmail.com>
2025-04-20 07:11:11 +0300

    4th commit

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 // filepath: /home/khalid-mabrux/documents/moringa-ft-13/phase-2/react-hooks-fetch-crud-lab/jest.config.js
 module.exports = {
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
-  transformIgnorePatterns: [
-    "/node_modules/(?!@adobe/css-tools)", // Allow Jest to transform @adobe/css-tools
-  ],
 };
