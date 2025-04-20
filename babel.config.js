61e720af5a4b995a1bc76e2cdd48255f04df8d92
Abel Lukwaro <abelmabruke@gmail.com>
2025-04-20 07:17:09 +0300

    5th commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+// filepath: /home/khalid-mabrux/documents/moringa-ft-13/phase-2/react-hooks-fetch-crud-lab/babel.config.js
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 // filepath: /home/khalid-mabrux/documents/moringa-ft-13/phase-2/react-hooks-fetch-crud-lab/jest.config.js
 module.exports = {
   transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@adobe/css-tools)", // Allow Jest to transform @adobe/css-tools
+  ],
 };
